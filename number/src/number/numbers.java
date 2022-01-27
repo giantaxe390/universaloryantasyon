0a65db91303d87ec7d0d3854aff0c89cb4226204
Mehmethan <Mehmethan@DESKTOP-SS6ONOH>
2022-01-27 15:40:45 +0300

    Updated

--- number/src/number/numbers.java
+++ number/src/number/numbers.java
@@ -6,8 +6,7 @@ public class numbers {
 
        public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		List<Integer> sayi=new ArrayList<Integer>();
-		int opp;
+		List<Integer> numbersList=new ArrayList<Integer>();
 		//Menu
 		while(true) {
 			System.out.println("Islem yapmak icin \n"
@@ -19,71 +18,51 @@ public class numbers {
 					+ "Menuden cikmak icin 0 tuslayin");
 			int menuNumber=sc.nextInt();
 			if(menuNumber==1) {
-				System.out.println("Faktoriyel almak icin bir sayi veriniz");
+				System.out.println("Faktoriyel almak icin bir numbersList veriniz");
 				int fact=sc.nextInt();
 				int mresult=factorial(fact);
 				System.out.println("Girilen sayinin faktoriyeli= "+mresult);
 			}
 			if(menuNumber==2) {
 				while(true) {
-					System.out.println("Arraye sayi eklemek icin sayi giriniz sonra 0 a tiklayin");
+					System.out.println("Arraye numbersList eklemek icin numbersList giriniz sonra 0 a tiklayin");
 					int arrNumb=sc.nextInt();	
 					if(arrNumb!=0) {
-						sayi.add(arrNumb);	
+						numbersList.add(arrNumb);	
 					}
 					else break;
 				}
 			}
 			if(menuNumber==3) {
-				if(sayi.isEmpty()) {
+				if(numbersList.isEmpty()) {
 					System.out.println("Listeyi doldurmaniz gerekli 2.adima donun");
 				}
 				else {
-					for(int i=0;i<sayi.size();i++) {
-						for(int j=i+1;j<sayi.size();j++) {
-							if(sayi.get(j)<sayi.get(i)) {
-								opp=sayi.get(i);
-								sayi.set(i, sayi.get(j));
-								sayi.set(j, opp);
-							}
-						}
-					}
-					System.out.println(sayi);
-					System.out.println("en kucuk"+sayi.get(0));
-					System.out.println("en buyuk"+sayi.get(sayi.size()-1));	
+					array_sort(numbersList);
+					System.out.println(numbersList);
+					System.out.println("en kucuk"+numbersList.get(0));
+					System.out.println("en buyuk"+numbersList.get(numbersList.size()-1));	
 				}
 				
 			}
 			if(menuNumber==4) {
-				if(sayi.isEmpty()) {
+				if(numbersList.isEmpty()) {
 					System.out.println("Listeyi doldurmaniz gerekli 2.adima donun");
 				}
 				else {
-					for(int i=0;i<sayi.size();i++) {
-						for(int j=i+1;j<sayi.size();j++) {
-							if(sayi.get(j)>sayi.get(i)) {
-								opp=sayi.get(i);
-								sayi.set(i, sayi.get(j));
-								sayi.set(j, opp);
-							}
-						}
-					}
-					System.out.println(sayi);
-					System.out.println("en buyuk= "+sayi.get(0));
-					System.out.println("en kucuk= "+sayi.get(sayi.size()-1));	
+					array_sortOpposite(numbersList);
+					System.out.println(numbersList);
+					System.out.println("en buyuk= "+numbersList.get(0));
+					System.out.println("en kucuk= "+numbersList.get(numbersList.size()-1));	
 				}
 			}
 			if(menuNumber==5) {
-				if(sayi.isEmpty()) {
+				if(numbersList.isEmpty()) {
 					System.out.println("Listeyi doldurmaniz gerekli 2.adima donun");
 				}
 				else {
-					for (int i = 0; i < sayi.size() / 2; i++) {
-					     int chosenNumb = sayi.get(i);
-					     sayi.set(i, sayi.get(sayi.size() - i - 1));
-					     sayi.set(sayi.size() - i - 1, chosenNumb);
-					   }
-					System.out.println(sayi);
+					array_reverse(numbersList);
+					System.out.println(numbersList);
 				}
 			}
 			if(menuNumber==0) {
@@ -92,7 +71,6 @@ public class numbers {
 		}
 		System.out.println("Basarili cikis yaptiniz");
 	}
-		
 		
 		
 	
@@ -104,6 +82,39 @@ public class numbers {
 		}
 		return result;
 	}
+	public static void array_sort(List<Integer> array){
+		int opp;
+		for(int i=0;i<array.size();i++) {
+			for(int j=i+1;j<array.size();j++) {
+				if(array.get(j)<array.get(i)) {
+					opp=array.get(i);
+					array.set(i, array.get(j));
+					array.set(j, opp);
+				}
+			}
+		}
+	}
+	public static void array_sortOpposite(List<Integer> array){
+		int opp;
+		for(int i=0;i<array.size();i++) {
+			for(int j=i+1;j<array.size();j++) {
+				if(array.get(j)>array.get(i)) {
+					opp=array.get(i);
+					array.set(i, array.get(j));
+					array.set(j, opp);
+				}
+			}
+		}
+	}
+	public static void array_reverse(List<Integer> array){
+		
+		for (int i = 0; i < array.size() / 2; i++) {
+		     int chosenNumb = array.get(i);
+		     array.set(i, array.get(array.size() - i - 1));
+		     array.set(array.size() - i - 1, chosenNumb);
+		   }
+	}
+	
 
 }
 
